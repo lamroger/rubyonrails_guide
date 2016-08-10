77e54360e612840d0636f5a5ff0241197a0baa7f
Roger Lam <mrlamroger@gmail.com>
2016-08-09 22:24:40 -0700

    add delete ability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def new
     @article = Article.new
@@ -28,12 +35,11 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def destroy
     @article = Article.find(params[:id])
-  end
+    @article.destroy
 
-  def index
-    @articles = Article.all
+    redirect_to articles_path
   end
 end
 
